782ff5b5b75553efd4b6c8c1bd14fec6eb9cf1aa
MiketheBite <migueljcpinto@mail.com>
2023-10-14 22:20:58 +0200

    sections done

---
+++ components/about.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+
+export default function About() {
+  return (
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
+      <SectionHeading>About me</SectionHeading>{" "}
+      <p className="mb-3">
+        After graduating with a degree in{" "}
+        <span className="font-medium">Nursing</span>, I decided to pursue my
+        passion for programming. I enrolled in a coding bootcamp at{" "}
+        <span className="font-medium text-orange-400	">Neuefische School </span>{" "}
+        and learned{" "}
+        <span className="font-medium">front-end web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect and the possibility of creating new and creative
+        things. I <span className="underline">love</span> the feeling of finally
+        figuring out a solution to a problem. My core stack is{" "}
+        <span className="font-medium">
+          React, Next.js, Node.js, and MongoDB
+        </span>
+        . I am also currently learning TypeScript and Prisma. I am always eager
+        to acquire new technologies. I am currently searching for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
+      </p>
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy
+        participating in sports, watching movies, and spending time with my
+        family. I also have a keen interest in{" "}
+        <span className="font-medium">learning new things</span> and I am
+        currently learning about{" "}
+        <span className="font-medium">politics and finances</span>.
+      </p>{" "}
+    </motion.section>
+  );
+}
--- lib/data.tsx
+++ lib/data.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
+import teamup from "@/public/teamup.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
@@ -62,11 +62,18 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Team´Up",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      '"TeamUp" is a Nurse Scheduling Application conceptualized as a capstone project for a Web Development Bootcamp.',
+    tags: [
+      "React",
+      "Next.js",
+      "MongoDB",
+      "Styled-Component",
+      "NextAuth.js",
+      "Node.js",
+    ],
+    imageUrl: teamup,
   },
   {
     title: "rmtDev",
