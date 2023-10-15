7d3c3c0b4630a4430cc36072b1c59f03a0075f37
MiketheBite <migueljcpinto@mail.com>
2023-10-16 00:33:49 +0200

    header animations

--- lib/data.tsx
+++ lib/data.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import teamup from "@/public/teamup.png";
-import rmtdevImg from "@/public/rmtdev.png";
+import teamupImg from "@/public/teamup.png";
+import popcornImg from "@/public/popcorn.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
@@ -62,6 +62,13 @@ export const experiencesData = [
 
 export const projectsData = [
   {
+    title: "PopCorn",
+    description:
+      "PopCorn is a movie app using OMDB API, allowing users to search, view, and curate watched movies with a IMDB-inspired UI.",
+    tags: ["React", "JavaScript", "Next.js", "Styled-Components", "Vercel"],
+    imageUrl: popcornImg,
+  },
+  {
     title: "Team´Up",
     description:
       '"TeamUp" is a Nurse Scheduling Application conceptualized as a capstone project for a Web Development Bootcamp.',
@@ -71,16 +78,9 @@ export const projectsData = [
       "MongoDB",
       "Styled-Component",
       "NextAuth.js",
-      "Node.js",
+      "Vercel",
     ],
-    imageUrl: teamup,
-  },
-  {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    imageUrl: teamupImg,
   },
   {
     title: "Word Analytics",
