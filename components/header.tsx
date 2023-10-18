a658c242231ba13f5001ffc9d45afa049cf398f1
MiketheBite <migueljcpinto@mail.com>
2023-10-18 11:24:26 +0200

    skills and header

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
 import clsx from "clsx";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Header() {
-  const [activeSection, setActiveSection] = useState("Home");
+  const { activeSection, setActiveSection, setTimeOfLastClick } =
+    useActiveSectionContext();
 
   return (
     <header className="z-[999] relative">
@@ -33,7 +35,9 @@ export default function Header() {
                   }
                 )}
                 href={link.hash}
-                onClick={() => setActiveSection(link.name)}
+                onClick={() => {
+                  setActiveSection(link.name), setTimeOfLastClick(Date.now());
+                }}
               >
                 {link.name}
 
