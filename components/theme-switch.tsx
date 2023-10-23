c205058f4b6f2c4dd4d066209130eb6ee35966f5
MiketheBite <migueljcpinto@mail.com>
2023-10-23 02:13:55 +0200

    darkmode

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,34 +1,15 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import { useTheme } from "@/context/theme-context";
+import React from "react";
 import { BsMoon, BsSun, BsWindowDesktop } from "react-icons/bs";
 
-type Theme = "light" | "dark";
-
 export default function ThemeSwitch() {
-  const [theme, setTheme] = useState<Theme>("light");
-
-  function toggleTheme() {
-    if (theme === "light") {
-      setTheme("dark");
-      window.localStorage.setItem("theme", "dark");
-    } else {
-      setTheme("light");
-      window.localStorage.setItem("theme", "light");
-    }
-  }
-
-  useEffect(() => {
-    const localTheme = window.localStorage.getItem("theme") as Theme | null;
-
-    if (localTheme) {
-      setTheme(localTheme);
-    }
-  }, []);
+  const { theme, toggleTheme } = useTheme();
 
   return (
     <button
-      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all"
+      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
       onClick={toggleTheme}
     >
       {" "}
